9f4ce12008ac6effb4b6dbfa1786999e85689e17
CamelGoong <goong0405@gmail.com>
2022-01-11 21:46:42 +0900

    Add files via upload

---
+++ Condition.c
@@ -0,0 +1,48 @@
+# include <stdio.h>
+
+int main(void){
+    
+    // 변수 설정
+    int counter = 1;
+    
+    // if문
+    if (counter < 10){
+        printf("x is less than 10\n");
+    }
+    else{
+        printf("x is not less than 10\n");
+    }
+
+    // while문
+    int i = 0;
+    
+    while ( i < 50)
+    {
+        printf("hello world\n");
+        i++;
+    }
+
+    // for문
+    for (int j =0; i< 50; i++)
+    {
+        printf("hello world\n");
+    }
+
+    // switch 문
+    int x = 3;
+
+    switch (x)
+    {
+        case 1:
+        printf("a\n");
+
+        case 2:
+        printf("b\n");
+
+        default:
+        printf("c\n");
+    }
+
+
+
+}
