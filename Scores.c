255fc314f991d9965cb9e21e8f3123672384949e
CamelGoong <goong0405@gmail.com>
2022-01-17 18:52:27 +0900

    Add files via upload

--- Scores.c
+++ Scores.c
@@ -1,13 +1,35 @@
 # include <cs50.h>
 # include <stdio.h>
 
+//const int N = 3; // 상수 : 3을 담은 변수 N을 만들고 싶지만, 그 값이 변하지는 않게 설정, 대문자로 변수명을 적는 것은 그냥 관
+//전역변수, 함수 밖에서 설정하는 것
+
+float average(int length, int array[]);
+//밑에서 정의한 average() 함수를 여기 사용했다는 것을 해줘야됨. 그래야지 멍청한 c언어가 알아들을 수 있음
+//;세미콜론으로 끝내는거 까먹지 말기
+
 int main(void)
 {
-    int scores[3]; //3개짜리 원소를 가지는 배열을 만들겠다.
+    int n = get_int("Number of scores: ");
 
-    scores[0] = 72;
-    scores[1] = 73;
-    scores[2] = 33;
+    int scores[n]; //크기가 n인 배열을 설정
 
-    printf("Average: %i\n",(scores[0] + scores[1] + scores[2]) / 3);
+    for(int i = 0; i <n; i++)
+    {
+        scores[i] = get_int("Score %i: ", i + 1);
+    }
+
+
+    printf("Average: %.2f\n",average(n,scores));
+}
+
+float average(int length, int array[]) //length와 array를 input으로 받아서 float을 output으로 반환하는 함수
+{
+    int sum = 0;
+    for(int i = 0; i < length; i ++)
+    {
+        sum += array[i];
+
+    }
+    return (float)sum / length; //int / int를 하면 int가 결과로 나옴, float이 나올 수 없음
 }
